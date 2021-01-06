041f4b80d6ac6f56a9098bd6358233f3836c3cfa
JohanvanAlphen <74180158+JohanvanAlphen@users.noreply.github.com>
2021-01-06 10:08:56 +0100

    Add files via upload

---
+++ Opdracht2_Kroegbaas/script.js
@@ -0,0 +1,20 @@
+const age = 17;
+if (age >= 18) {
+    console.log("Gezellig, je mag naar binnen :)");
+} else if (age < 18) {
+    console.log("Helaas, je bent nog te jong om naar binnen te mogen :(");
+}
+
+const isFemale = false;
+if (isFemale) {
+    console.log("Het is Ladiesnight, dus kom er in :))");
+} else {
+    console.log("Het is vandaag Ladiesnight, dus helaas geen entree voor heren :((");
+}
+
+const driverStatus = 'Henk'
+if (driverStatus === 'Bob') {
+    console.log("Goed gedaan, jij bent de Bob en mag gaan rijden :)))");
+} else {
+    console.log("Helaas, je hebt teveel gedronken en mag geen auto meer rijden ;)");
+}
